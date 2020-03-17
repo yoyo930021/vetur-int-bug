8aca4d75db1b28b9728bc603c45d6b37e113c3ea
yoyo930021 <yoyo930021@gmail.com>
2020-03-17 14:53:35 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <ul
+      v-for="group in groups"
+      :key="group.id"
+    >
+      <li>
+        <ul
+          v-for="item in group.list"
+          :key="item"
+        >
+
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const groups = computed(() => {
+      const result = []
+      result.push({
+        id: 1,
+        text: '',
+        list: []
+      })
+
+      return result
+    });
+
+    return {
+      groups
+    };
+  }
+});
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,5 @@
+<script lang="ts">
+import __vlsComponent from "./HelloWorld.vue";
+import { __vlsRenderHelper, __vlsComponentHelper, __vlsIterationHelper } from "vue-editor-bridge";
+__vlsRenderHelper(__vlsComponent, function () { __vlsComponentHelper(this, "div", { props: {}, on: {}, directives: [] }, [__vlsIterationHelper(this.groups, (group) => __vlsComponentHelper(this, "ul", { props: { "key": group.id }, on: {}, directives: [] }, [__vlsComponentHelper(this, "li", { props: {}, on: {}, directives: [] }, [__vlsIterationHelper(group.list, (item) => __vlsComponentHelper(this, "ul", { props: { "key": item }, on: {}, directives: [] }, []))])]))]); });
+</script>
